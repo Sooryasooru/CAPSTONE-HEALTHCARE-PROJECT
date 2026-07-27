d8a36f18bbcb980ff3fb44218ea89e2cd8cc9d70
Soorya <soorya0286@gmail.com>
2026-07-27 05:29:45 +0000

    fix: prune disk and force-recreate on deploy

--- scripts/start.sh
+++ scripts/start.sh
@@ -2,12 +2,8 @@
 set -e
 cd /home/ubuntu/CAPSTONE-HEALTHCARE-PROJECT
 
-AWS_REGION=eu-north-1
-ACCOUNT_ID=700640309357
-
-# Authenticate Docker to ECR (EC2 role provides credentials)
-aws ecr get-login-password --region $AWS_REGION \
-  | docker login --username AWS --password-stdin $ACCOUNT_ID.dkr.ecr.$AWS_REGION.amazonaws.com
+# Free disk before pulling new images (prevents "no space left")
+docker system prune -af || true
 
 docker compose -f docker-compose.prod.yml pull
 docker compose -f docker-compose.prod.yml up -d --force-recreate
